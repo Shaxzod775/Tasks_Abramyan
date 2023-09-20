152bac461018d1c079193c04760f65b8171cd30a
Shaxzod775 <134335101+Shaxzod775@users.noreply.github.com>
2023-09-20 10:36:47 +0500

    Add files via upload

---
+++ for.py
@@ -0,0 +1,45 @@
+# # For5◦ Дано вещественное число — цена 1 кг конфет.
+# # Вывести стоимость 0.1, 0.2, . . . , 1 кг конфет.
+#
+# def price_of_part():
+#     price_per_kg = float(input("Введите цену 1 кг конфет: "))
+#
+#     for i in range(1, 11):
+#         weight = i / 10
+#         cost = price_per_kg * weight
+#         print(f"{weight} кг конфет стоят {cost} сум.")
+#
+# price_of_part()
+
+
+
+# For4◦ Дано вещественное число — цена 1 кг конфет. Вывести стоимость 1,
+# 2, . . . , 10 кг конфет
+
+# def price_of_kg():
+#     price_per_kg = int(input("Введите цену 1 кг конфет: "))
+#
+#     for i in range(1, 11):
+#         weight = i + 1
+#         cost = price_per_kg * weight
+#         print(f"{weight} кг конфет стоят {cost} сум.")
+#
+# price_of_part()
+
+
+# For6. Дано вещественное число — цена 1 кг конфет. Вывести стоимость 1.2,
+# 1.4, . . . , 2 кг конфет.
+
+# def price_of_part():
+#     price_per_kg = float(input("Введите цену 1 кг конфет: "))
+#
+#     for i in range(1, 11):
+#         weight = i / 10
+#         cost = price_per_kg * weight
+#         print(f"{weight} кг конфет стоят {cost} сум.")
+#
+# price_of_part()
+
+
+
+
